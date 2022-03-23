c4d8d54c780c1af66a8669147df8a07e3e45b42e
Karanu Newton <karanunewton4@gmail.com>
2022-03-23 13:24:34 +0300

    WIP - implement logout functionality

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService : AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
 
 }
