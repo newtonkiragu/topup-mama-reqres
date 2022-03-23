a4b85809b1a8924c0110199ff93868c0503f3c30
Karanu Newton <karanunewton4@gmail.com>
2022-03-23 17:16:17 +0300

    refactored authentication service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
-import {AuthService} from "../auth.service";
+import {AuthService} from "../auth/auth.service";
 import {Router} from "@angular/router";
 
 @Component({
