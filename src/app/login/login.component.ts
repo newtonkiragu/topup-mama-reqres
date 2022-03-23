1f76800467e768755a6794220bd7d3fdc7eaa81a
Karanu Newton <karanunewton4@gmail.com>
2022-03-23 13:21:03 +0300

    WIP - setup login component and implement login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userName: string;
+  password: string;
+  formData: FormGroup;
 
-  constructor() { }
+  constructor(private authService : AuthService, private router : Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.formData = new FormGroup({
+      userName: new FormControl("admin"),
+      password: new FormControl("admin"),
+    });
   }
 
+  onClickSubmit(data: any) {
+    this.userName = data.userName;
+    this.password = data.password;
+
+    console.log("Login page: " + this.userName);
+    console.log("Login page: " + this.password);
+
+    this.authService.login(this.userName, this.password)
+      .subscribe( data => {
+        console.log("Is Login Success: " + data);
+
+        if(data) this.router.navigate(['/']);
+      });
+  }
 }
