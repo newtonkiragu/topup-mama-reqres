b801b343deaa387b751b420384f8c10c0230eea7
Karanu Newton <karanunewton4@gmail.com>
2022-03-26 20:53:24 +0300

    refactored location of services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "./services/auth/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'topup-mama-reqres';
+  isUserLoggedIn = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    let storeData = localStorage.getItem("isUserLoggedIn");
+    console.log("StoreData: " + storeData);
+
+    if( storeData != null && storeData == "true")
+      this.isUserLoggedIn = true;
+    else
+
+
+      this.isUserLoggedIn = false;
+  }
 }
